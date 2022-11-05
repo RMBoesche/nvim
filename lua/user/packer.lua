b5e6c713e433b00edaccf3991e78f92f5805db8f
RMBoesche <rodrigo.boesche@gmail.com>
2022-11-05 16:01:19 -0300

    git init

---
+++ lua/user/packer.lua
@@ -0,0 +1,11 @@
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use 'lunarvim/colorschemes'
+  use 'EdenEast/nightfox.nvim'
+  use 'folke/tokyonight.nvim'
+  use 'Mofiqul/dracula.nvim'
+  use 'shaunsingh/nord.nvim'
+  use 'overcache/NeoSolarized'
+  use 'ellisonleao/gruvbox.nvim'
+end)
