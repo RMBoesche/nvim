dafc35eb9ff8d413bfe10891c71a034208589fd8
RMBoesche <rodrigo.boesche@gmail.com>
2022-11-07 23:55:11 -0300

    snips and cmp

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -7,6 +7,7 @@ return require('packer').startup(function(use)
   use 'folke/tokyonight.nvim'
   use 'Mofiqul/dracula.nvim'
   use 'shaunsingh/nord.nvim'
+  use "lunarvim/darkplus.nvim"
   -- use 'overcache/NeoSolarized'
   -- use 'ellisonleao/gruvbox.nvim'
   -- use 'tanvirtin/monokai.nvim'
@@ -52,5 +53,16 @@ return require('packer').startup(function(use)
       require('Comment').setup()
     end
   }
+
+  -- CMP plugins
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+
+  -- Snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to usek
 
 end)
